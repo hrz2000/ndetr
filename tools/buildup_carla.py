c7aa31817b8cb5a7eb225f609f76316408f7c486
hrz-debug <1904486494@qq.com>
2023-02-17 17:17:54 +0800

    xx

--- tools/buildup_carla.py
+++ tools/buildup_carla.py
@@ -4,5 +4,5 @@ import time
 for idx, port in enumerate(range(4)):
     cuda = idx % 4
     port = f"{port}0"
-    p = subprocess.Popen(f"CUDA_VISIBLE_DEVICES={cuda} SDL_VIDEODRIVER=offscreen $CARLA_SERVER -carla-rpc-port=20{port} -nosound -opengl", shell=True)
+    p = subprocess.Popen(f"CUDA_VISIBLE_DEVICES={cuda} SDL_VIDEODRIVER=offscreen $CARLA_SERVER -carla-rpc-port=20{port} -nosound -opengl 2>&1|tee script/_carla{idx}.log", shell=True)
     time.sleep(3)
